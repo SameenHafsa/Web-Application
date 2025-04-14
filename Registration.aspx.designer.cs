0dabec0369265a9914009d97a6ccff0ef31b3629
Sameen <150486455+SameenHafsa@users.noreply.github.com>
2025-04-14 17:30:41 -0500

    Update Registration.aspx.designer.cs

--- Registration.aspx.designer.cs
+++ Registration.aspx.designer.cs
@@ -4,8 +4,7 @@
 //
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated. 
-// </auto-generated>
-//------------------------------------------------------------------------------
+// </auto-generated>--
 
 namespace APA_Sameen.APA
 {
